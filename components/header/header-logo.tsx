d342fd1cd9740d0b92de356412e725e2f31fd4ba
viettran27 <viettranduc8996@gmail.com>
2024-06-16 15:19:48 +0700

    create header logo

---
+++ components/header/header-logo.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+const HeaderLogo = () => {
+  return ( 
+    <div className="hidden md:block">
+      <Link href={"/"}>
+        <div className="text-2xl font-bold">Shop VM</div>
+      </Link>
+    </div>
+   );
+}
+
+export default HeaderLogo
