66e7139b2b67a449c88c269f5df5d731ad8ebe77
viettran27 <viettranduc8996@gmail.com>
2024-06-17 17:00:50 +0700

    create customer review component

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -2,16 +2,18 @@ import ProductCarousel from '@/components/product-carousel'
 import Banner from '@/features/home/components/banner'
 import Brand from '@/features/home/components/brand'
 import BrowserStyle from '@/features/home/components/browser-style'
+import CustomerReview from '@/features/home/components/customer-review'
 
 const Home = () => {
   return (
-    <div className="space-y-12">
+    <div className="space-y-12 pb-20">
       <div>
         <Banner />
         <Brand />
       </div>
       <ProductCarousel title="New Arrivals" />
       <BrowserStyle />
+      <CustomerReview />
     </div>
   )
 }
---
+++ features/home/components/customer-review.tsx
@@ -0,0 +1,48 @@
+import MaxWidthContent from '@/components/max-width-content'
+import Review from '@/components/review'
+import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from '@/components/ui/carousel'
+
+const REVIEWS = [
+  {
+    name: 'Sara Smith',
+    content:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    rating: 5
+  },
+  {
+    name: 'Sara Smith',
+    content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, quae.',
+    rating: 5
+  },
+  {
+    name: 'Sara Smith',
+    content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, quae.',
+    rating: 5
+  },
+  {
+    name: 'Sara Smith',
+    content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, quae.',
+    rating: 5
+  }
+]
+
+const CustomerReview = () => {
+  return (
+    <MaxWidthContent>
+      <div className="text-2xl md:text-3xl font-bold">OUR HAPPY CUSTOMERS</div>
+      <Carousel className="mt-8">
+        <CarouselContent>
+          {REVIEWS.map((review, index) => (
+            <CarouselItem key={index} className="basis-full md:basis-1/2 lg:basis-1/3">
+              <Review className="max-w-full" userName={review.name} review={review.content} rating={review.rating} />
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious className="-top-[52px] right-[42px] left-auto" />
+        <CarouselNext className="-top-[52px] right-0" />
+      </Carousel>
+    </MaxWidthContent>
+  )
+}
+
+export default CustomerReview
