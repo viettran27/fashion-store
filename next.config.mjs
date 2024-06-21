6a27ee7f9e39cc23df827fecef3605a6b5721ae3
viettran27 <viettranduc8996@gmail.com>
2024-06-21 15:42:16 +0700

    fix config app

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['s3-alpha-sig.figma.com']
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 's3-alpha-sig.figma.com',
+        pathname: '/**'
+      }
+    ]
   }
 }
 
